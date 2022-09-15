cd61a22650627f13036872c7eee1533b14a382be
xlrtamania <83196575+xlrtamania@users.noreply.github.com>
2022-09-15 05:48:28 +0000

    Update user.js

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 
 const mongoose =require("mongoose")
 main().catch(err => console.log(err))
-//here we define our database schema
+//here we define our schema
 const userSchema=new mongoose.Schema({
 	name:String,
 	birth_year:Number,
@@ -9,6 +9,7 @@ const userSchema=new mongoose.Schema({
 	password:String,
 	words:String
 })
+//connecting to a local mongodb database with no authentication
 async function main(){
 await mongoose.connect('mongodb://localhost:27017/test')}
 module.exports=mongoose.model("user",userSchema)
